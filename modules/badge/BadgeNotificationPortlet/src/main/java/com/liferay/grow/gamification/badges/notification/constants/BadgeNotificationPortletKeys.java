32fc94bc07a23e10a346766ecf81371755a0eed2
Vilmos Papp <vilmos.papp@liferay.com>
2019-11-14 11:44:18 +0100

    GROW-945 Changed logic to retreive userName

--- modules/badge/BadgeNotificationPortlet/src/main/java/com/liferay/grow/gamification/badges/notification/constants/BadgeNotificationPortletKeys.java
+++ modules/badge/BadgeNotificationPortlet/src/main/java/com/liferay/grow/gamification/badges/notification/constants/BadgeNotificationPortletKeys.java
@@ -7,6 +7,8 @@ public class BadgeNotificationPortletKeys {
 
 	public static final String BADGE_COMMENT = "BADGE_COMMENT";
 
+	public static final String BADGE_SENDER = "BADGE_SENDER";
+
 	public static final String BADGE_TYPE = "BADGE_TYPE";
 
 	public static final String BadgeNotification = "badgenotification";
